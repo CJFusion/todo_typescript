e4b2485217a54595d84ce022f72609d82b0fc86e
Audrian Marak <undrumarak12@gmail.com>
2024-02-06 21:36:05 +0530

    Added operations for todo list
    => Used states for todo list
    => Created functions for adding, updating & deleting todo item

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,17 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./styles.css";
 import { NewTodoForm } from "./NewTodoForm";
 import { TodoList } from "./TodoList";
 import { TodoItemProps } from "./TodoItem";
 
 export default function App() {
-	const [todos, setTodos] = useState(() => {});
+	const [todos, setTodos] = useState<TodoItemProps[]>([]);
 
-	function addTodo(title: string) {}
+	function addTodo(title: string) {
+		setTodos((currentTodos: TodoItemProps[]) => {
+			return [
+				...currentTodos,
+				{ id: crypto.randomUUID(), title, completed: false },
+			];
+		});
+	}
 
-	function toggleTodo(id: number, completed: boolean) {}
+	function toggleTodo(id: number, completed: boolean) {
+		setTodos((currentTodos: TodoItemProps[]) => {
+			return currentTodos.map((todo) => {
+				if (todo.id === id) {
+					return { ...todo, completed };
+				}
 
-	function deleteTodo(id: number) {}
+				return todo;
+			});
+		});
+	}
+
+	function deleteTodo(id: number) {
+		setTodos((currentTodos: TodoItemProps[]) => {
+			return currentTodos.filter((todo) => todo.id !== id);
+		});
+	}
 
 	return (
 		<>
