e67341b9d3bca7f8cd246a3a53884429d8fbdd2a
Audrian Marak <undrumarak12@gmail.com>
2024-02-06 20:18:37 +0530

    Created basic layout
    => Added form for adding Todo Item
    => Created unordered list of items (template)

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+
 function App() {
-	return <>Hi</>;
+	const [newItem, setNewItem] = useState("");
+
+	return (
+		<>
+			<form onSubmit={() => {}}>
+				<div>
+					<label htmlFor="item">New Item</label>
+					<input
+						value={newItem}
+						onChange={() => {}}
+						type="text"
+						id="item"
+					/>
+				</div>
+				<button>Add</button>
+			</form>
+
+			<h1>Todo List</h1>
+			<ul>
+				<li>
+					<label>
+						<input
+							type="checkbox"
+							// checked={false}
+							onChange={() => {}}
+						/>
+						{"Todo Item Title"}
+					</label>
+					<button onClick={() => {}}>Delete</button>
+				</li>
+			</ul>
+		</>
+	);
 }
 
 export default App;
